cc9e88e7f1feddc724866497de9cf3e9a347fcac
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2025-08-19 15:05:38 +0800

    update banner

--- src/config.ts
+++ src/config.ts
@@ -17,12 +17,12 @@ export const siteConfig: SiteConfig = {
   },
   banner: {
     enable: true,
-    src: 'assets/images/demo-banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center', // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    src: 'assets/images/banner.jpg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'top', // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false, // Display the credit text of the banner image
-      text: '', // Credit text to be displayed
-      url: '', // (Optional) URL link to the original artwork or artist's page
+      enable: true, // Display the credit text of the banner image
+      text: 'baek_hyang / Furina & Rafale', // Credit text to be displayed
+      url: 'https://www.pixiv.net/artworks/128209639', // (Optional) URL link to the original artwork or artist's page
     },
   },
   toc: {
@@ -86,7 +86,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'QQ',
       icon: 'fa6-brands:qq',
-      url: 'https://github.com/misaka10987',
+      url: 'http://2208129531.qzone.qq.com',
     },
     {
       name: '微信',
